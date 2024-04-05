acfef78651fbbfb8c2793bb4c442f1a5149527ea
= <=>
2024-04-05 21:25:15 +0500

    Covered the following point " - The service should send a message to the operations topic if any exceptions occur during processing. "

--- FinancialTrackingSystemMain/FinancialTrackingSystemMain/Program.cs
+++ FinancialTrackingSystemMain/FinancialTrackingSystemMain/Program.cs
@@ -9,46 +9,53 @@ namespace FinancialTrackingSystemMain
     {
         static void Main(string[] args)
         {
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
-            factory.ClientProvidedName = "Financial Transaction Tracking App";
-
-            IConnection connection = factory.CreateConnection();    
-            IModel channel = connection.CreateModel();
-
-            string exchangeName = "amq.topic";
-            string routingkey = "rabbitmq.financial.transaction.routingkey";
-            string queueName = "queue.api.financial.app";
-
-            channel.ExchangeDeclare(exchangeName, ExchangeType.Topic, true);
-            channel.QueueDeclare(queueName, true, false, false, null);
-            channel.QueueBind(queueName, exchangeName, routingkey, null);
-
-            var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += (sender, args) =>
+            try
             {
-                var body = args.Body.ToArray();
-                string message = Encoding.UTF8.GetString(body);
-                // deserialize the message
-                var transaction = Mapper.GetDeserializedTransaction(message);
-                if(transaction != null)
+                ConnectionFactory factory = new ConnectionFactory();
+                factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+                factory.ClientProvidedName = "Financial Transaction Tracking App";
+
+                IConnection connection = factory.CreateConnection();
+                IModel channel = connection.CreateModel();
+
+                string exchangeName = "amq.topic";
+                string routingkey = "rabbitmq.financial.transaction.routingkey";
+                string queueName = "queue.api.financial.app";
+
+                channel.ExchangeDeclare(exchangeName, ExchangeType.Topic, true);
+                channel.QueueDeclare(queueName, true, false, false, null);
+                channel.QueueBind(queueName, exchangeName, routingkey, null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (sender, args) =>
                 {
-                    var validator = new Validator();
-                    var isValidtransaction = validator.Validate(transaction);
-                    if (isValidtransaction)
-                        RabbitMQService.PublishToConsumer("queue.api.processing.transaction", "rabbitmq.financial.sender.processing.routingkey", message);
-                    else
-                        RabbitMQService.PublishToConsumer("queue.api.holding.transaction", "rabbitmq.financial.sender.holding.routingkey", message);
-                    channel.BasicAck(args.DeliveryTag, false);
-                }
-            };
+                    var body = args.Body.ToArray();
+                    string message = Encoding.UTF8.GetString(body);
+                    // deserialize the message
+                    var transaction = Mapper.GetDeserializedTransaction(message);
+                    if (transaction != null)
+                    {
+                        var validator = new Validator();
+                        var isValidtransaction = validator.Validate(transaction);
+                        if (isValidtransaction)
+                            Service.RabbitMQ.PublishToConsumer("queue.api.processing.transaction", "rabbitmq.financial.sender.processing.routingkey", message);
+                        else
+                            Service.RabbitMQ.PublishToConsumer("queue.api.holding.transaction", "rabbitmq.financial.sender.holding.routingkey", message);
+                        channel.BasicAck(args.DeliveryTag, false);
+                    }
+                };
 
-            string consumerTag = channel.BasicConsume(queueName, false, consumer);
-            Console.ReadLine();
+                string consumerTag = channel.BasicConsume(queueName, false, consumer);
+                Console.ReadLine();
 
-            channel.BasicCancel(consumerTag);
-            channel.Close();
-            connection.Close();
+                channel.BasicCancel(consumerTag);
+                channel.Close();
+                connection.Close();
+            }
+            catch(Exception ex)
+            {
+                Service.RabbitMQ.PublishException(ex.Message.ToString());
+            }
         }
     }
 }
---
+++ FinancialTrackingSystemMain/FinancialTrackingSystemMain/Service/RabbitMQ.cs
@@ -0,0 +1,67 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialTrackingSystemMain.Service
+{
+    public class RabbitMQ
+    {
+        public static void PublishToConsumer(string queuename, string routing, string message)
+        {
+            try
+            {
+                ConnectionFactory factory = new ConnectionFactory();
+                factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+                factory.ClientProvidedName = "Financial Transaction Tracking App";
+
+                IConnection connection = factory.CreateConnection();
+                IModel channel = connection.CreateModel();
+
+                string exchangeName = "amq.direct";
+                string routingKey = routing;
+                string queueName = queuename;
+
+                channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+                channel.QueueDeclare(queueName, false, false, false, null);
+                channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+                byte[] messageBodyBytes = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+
+                channel.Close();
+                connection.Close();
+            }
+            catch(Exception ex) 
+            {
+                PublishException(ex.Message.ToString());
+            }
+        }
+
+        public static void PublishException(string error)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+            factory.ClientProvidedName = "Financial Transaction Tracking App";
+
+            IConnection connection = factory.CreateConnection();
+            IModel channel = connection.CreateModel();
+
+            string exchangeName = "amq.direct";
+            string routingKey = "rabbitmq.financial.sender.error.routingkey";
+            string queueName = "queue.api.error.transaction";
+
+            channel.ExchangeDeclare(exchangeName, ExchangeType.Direct, true);
+            channel.QueueDeclare(queueName, false, false, false, null);
+            channel.QueueBind(queueName, exchangeName, routingKey, null);
+
+            byte[] messageBodyBytes = Encoding.UTF8.GetBytes(error);
+            channel.BasicPublish(exchangeName, routingKey, null, messageBodyBytes);
+
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
